ed93fcdbf7f565edb91159a87f8a36227dd78d96
Alicia <37165165+YaqianQi@users.noreply.github.com>
2020-03-21 14:54:33 -0400

    Add files via upload

---
+++ Python Basic/Week 2/Quiz/Logical operators_1.py
@@ -0,0 +1,9 @@
+
+num1 = int(input())
+num2 = int(input())
+res = 0
+if num1 > num2:
+    res = num1
+else:
+    res = num2
+print(int(res))
---
+++ Python Basic/Week 2/Quiz/While loop practice_1.py
@@ -0,0 +1,5 @@
+
+num = int(input())
+while num <= 100:
+    print(num ** 3)
+    num += 1
