1e0d39dd4d597d8d6ce97a0f553af95071a10be5
Alicia <37165165+YaqianQi@users.noreply.github.com>
2020-03-21 14:45:25 -0400

    Add files via upload

---
+++ Python Basic/Week 2/Conditional Statement Practice_1.py
@@ -0,0 +1,6 @@
+num = int(input())
+mod = num % 2
+if mod > 0:
+    print('odd')
+else:
+    print('even')
---
+++ Python Basic/Week 2/Logical operators_2.py
@@ -0,0 +1,5 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+res = min(num1, num2, num3)
+print(int(res))
