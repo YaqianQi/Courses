54f28716312f06d65d23f1fb0b1ecedc31b36188
Alicia <37165165+YaqianQi@users.noreply.github.com>
2023-02-18 17:19:30 -0500

    format backup.sh

--- hse-hw/DevOps/backup-new.sh
+++ hse-hw/DevOps/backup-new.sh
@@ -10,50 +10,67 @@
 # 
 # All output except errors will be written to the error.log file instead of stderr.
 #
-# ./backup.sh <backup_dir>  <compression_algorithm>  <output_file_name>
+# ./backup.sh <BACKUP_DIR>  <COMPRESSION_ALGO>  <OUTPUT_FILE_NAME>
+# 
 # decrypt file
-# openssl aes-256-cbc -salt -a -d -in $output_file_name.aes128 -out $full_output_name
+# openssl aes-256-cbc -salt -a -d -in $OUTPUT_FILE_NAME.aes128 -out $ARCHIVE_FILE
 ####################################
 
-# Accept three parameters
-backup_dir=$1
-compression_algorithm=$2
-output_file_name=$3
-# Set the path to the error log file
-error_log="error.log"
-full_output_name=""
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+	echo "You have failed to pass a parameter."
+	echo "USAGE: ./backup.sh BACKUP_DIR COMPRESSION_ALGO OUTPUT_FILE_NAME"
+	exit 255;
+fi
 
-# Create the backup archive with the specified compression algorithm
+# Accept three parameters
+BACKUP_DIR=$1
+COMPRESSION_ALGO=$2
+OUTPUT_FILE_NAME=$3
+
+# Set the path to the error log file
+ERROR_LOG="error.log"
+
+# Output compression output name with file file type 
+ARCHIVE_FILE=""
+
+# Supported compression algo
 TAR="tar"
 BZIP="bzip"
 BZIP2="bzip2"
 XZ="xz"
 
 
-if [[ "$compression_algorithm" == "$TAR"  ]]; then
-  full_output_name="$output_file_name.tar.gz"
-  tar  -czvf full_output_name $backup_dir 2>> $error_log; 
-  echo "tar"
-elif [[ "$compression_algorithm" == "$BZIP" ]] || [[ "$compression_algorithm" == "$BZIP2" ]]; then
-  full_output_name="$output_file_name.tar.bz2"
-  tar  -czvf full_output_name $backup_dir 2>> $error_log; 
-  echo "biz"
-elif [[ "$compression_algorithm" == "$XZ"  ]]; then
-  full_output_name="$output_file_name.tar.xz"
-  tar -cJvf full_output_name $backup_dir 2>> $error_log; 
-  echo "xz"
+if [ ! -d $BACKUP_DIR ]; then
+	(echo "directory not exist") >& $LOGFILE
+	echo "exit"
+	exit 1
+fi
+
+
+# Create the backup archive with the specified compression algorithm
+if [[ "$COMPRESSION_ALGO" == "$TAR"  ]]; then
+  ARCHIVE_FILE="$OUTPUT_FILE_NAME.tar.gz"
+  tar  -czvf ARCHIVE_FILE $BACKUP_DIR 2>> $ERROR_LOG; 
+
+elif [[ "$COMPRESSION_ALGO" == "$BZIP" ]] || [[ "$COMPRESSION_ALGO" == "$BZIP2" ]]; then
+  ARCHIVE_FILE="$OUTPUT_FILE_NAME.tar.bz2"
+  tar  -czvf ARCHIVE_FILE $BACKUP_DIR 2>> $ERROR_LOG; 
+
+elif [[ "$COMPRESSION_ALGO" == "$XZ"  ]]; then
+  ARCHIVE_FILE="$OUTPUT_FILE_NAME.tar.xz"
+  tar -cJvf ARCHIVE_FILE $BACKUP_DIR 2>> $ERROR_LOG; 
+
 else 
-echo "Invalid compression option." 2>> $error_log;
+  echo "Invalid compression option." 2>> $ERROR_LOG;
 fi 
 
 
-
-openssl aes-256-cbc -salt -a -e -in $full_output_name -out $output_file_name.aes128 2>> $error_log;
-# openssl aes-256-cbc -salt -a -d -in $output_file_name.aes128 -out $full_output_name 2>> $error_log;
+# Encryption
+openssl aes-256-cbc -salt -a -e -in $ARCHIVE_FILE -out $OUTPUT_FILE_NAME.aes128 2>> $ERROR_LOG;
 
 # Check if the backup archive was created successfully
 if [ $? -eq 0 ]; then
-  echo "Backup archive:$full_output_name created successfully"; 
+  echo "Backup archive:$ARCHIVE_FILE created successfully"; 
 else
-  echo "Error creating backup archive. Please check $error_log for more information."
+  echo "Error creating backup archive. Please check $ERROR_LOG for more information." 2>> $ERROR_LOG;
 fi
